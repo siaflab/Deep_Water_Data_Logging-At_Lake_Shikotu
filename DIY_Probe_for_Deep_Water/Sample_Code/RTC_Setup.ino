2ccaae60751f24f2358ded1e49b2c3bc359ee05e
keikomachiya <komachiya@gmail.com>
2024-11-12 16:32:56 +0900

    Create RTC_Setup.ino

---
+++ DIY_Probe_for_Deep_Water/Sample_Code/RTC_Setup.ino
@@ -0,0 +1,35 @@
+#include <Wire.h>
+#include "RTClib.h"
+
+RTC_DS1307 rtc;
+
+void setup () {
+  while (!Serial);
+  Serial.begin(9600);
+  if (! rtc.begin()) {
+    Serial.println("No RTC found");
+    while (1);
+  }
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is not configured");
+  }
+  // Set Date and Time (Year, DAY, Month, Hour(24h), Minute, Second)
+  rtc.adjust(DateTime(2024, 11, 11, 1, 11, 11)); //Uncomment this line to set Date and Time
+}
+
+void loop () { // Get Date and Time
+  DateTime now = rtc.now();
+  Serial.print(now.year(), DEC);
+  Serial.print('-');
+  Serial.print(now.month(), DEC);
+  Serial.print('-');
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+  delay(3000);
+}
